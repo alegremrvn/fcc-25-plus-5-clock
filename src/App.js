a7a3072ff1008226501d4d58a38b77bef435efc1
Marvin Alegre <alegremrvn@protonmail.com>
2022-07-17 17:30:50 +0800

    created a react component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from 'react';
 import './App.css';
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div>
-      
+      <Clock />
     </div>
   );
 }

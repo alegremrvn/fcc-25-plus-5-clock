975206c4649e282546c6001b3f0a974da37e8bc7
Marvin Alegre <alegremrvn@protonmail.com>
2022-07-17 17:48:37 +0800

    added all necessary html elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import beep from './Bomb-SoundBible.com-891110113.mp3';
 
 class Clock extends React.Component {
   constructor(props) {
@@ -12,7 +13,28 @@ class Clock extends React.Component {
   render() {
     return (
       <div>
-        
+        <div>
+          <h2 id="break-label">Break Length</h2>
+          <p id="break-length"></p>
+          <button id="break-decrement">decrement</button>
+          <button id="break-increment">increment</button>
+        </div>
+
+        <div>
+          <h2 id="session-label">Session Length</h2>
+          <p id="session-length"></p>
+          <button id="session-decrement">decrement</button>
+          <button id="session-increment">increment</button>
+        </div>
+
+        <div>
+          <h2 id="timer-label"></h2>
+          <p id="time-left"></p>
+          <button id="start_stop">start-stop</button>
+          <button id="reset">reset</button>
+        </div>
+
+        <audio id="beep" src={beep}></audio>
       </div>
     );
   }

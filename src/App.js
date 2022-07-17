60389c9bc18ab77356f97700f3b52139eb6f1ded
Marvin Alegre <alegremrvn@protonmail.com>
2022-07-17 18:06:45 +0800

    installed all handler functions

--- src/App.js
+++ src/App.js
@@ -8,6 +8,36 @@ class Clock extends React.Component {
     this.state = {
 
     };
+    this.handleBreakDecrement = this.handleBreakDecrement.bind(this);
+    this.handleBreakIncrement = this.handleBreakIncrement.bind(this);
+    this.handleSessionDecrement = this.handleSessionDecrement.bind(this);
+    this.handleSessionIncrement = this.handleSessionIncrement.bind(this);
+    this.handleStartStop = this.handleStartStop.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleBreakDecrement() {
+
+  }
+
+  handleBreakIncrement() {
+
+  }
+
+  handleSessionDecrement() {
+
+  }
+
+  handleSessionIncrement() {
+
+  }
+
+  handleStartStop() {
+
+  }
+
+  handleReset() {
+
   }
 
   render() {
@@ -16,22 +46,22 @@ class Clock extends React.Component {
         <div>
           <h2 id="break-label">Break Length</h2>
           <p id="break-length"></p>
-          <button id="break-decrement">decrement</button>
-          <button id="break-increment">increment</button>
+          <button id="break-decrement" onClick={this.handleBreakDecrement}>decrement</button>
+          <button id="break-increment" onClick={this.handleBreakIncrement}>increment</button>
         </div>
 
         <div>
           <h2 id="session-label">Session Length</h2>
           <p id="session-length"></p>
-          <button id="session-decrement">decrement</button>
-          <button id="session-increment">increment</button>
+          <button id="session-decrement" onClick={this.handleSessionDecrement}>decrement</button>
+          <button id="session-increment" onClick={this.handleSessionIncrement}>increment</button>
         </div>
 
         <div>
           <h2 id="timer-label"></h2>
           <p id="time-left"></p>
-          <button id="start_stop">start-stop</button>
-          <button id="reset">reset</button>
+          <button id="start_stop" onClick={this.handleStartStop}>start-stop</button>
+          <button id="reset" onClick={this.handleReset}>reset</button>
         </div>
 
         <audio id="beep" src={beep}></audio>
